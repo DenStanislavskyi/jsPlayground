31e9b8830bf851d3a4dc27bd50e62adc3b6f30b6
Denys Stanislavskyi <31162509+DenStanislavskyi@users.noreply.github.com>
2021-10-29 15:59:21 +0200

    Create exerciseStopWatchWithPrototype.js

---
+++ exerciseStopWatchWithPrototype.js
@@ -0,0 +1,48 @@
+// exerciseStopWatchWithPrototype 
+// modification of duration is possible from the outside, making this object useless and untrustworthy
+
+function Stopwatch(){
+    let startTime, endTime, running, duration = 0;
+
+    Object.defineProperty(this, 'duration', {
+        get: function() {return duration;},
+        set: function(value) {return duration = value;}
+    });
+    Object.defineProperty(this, 'startTime', {
+        get: function() {return startTime;}
+    });
+    Object.defineProperty(this, 'endTime', {
+        get: function() {return endTime;}
+    });
+    Object.defineProperty(this, 'running', {
+        get: function() {return running;}
+    });
+}
+
+Stopwatch.prototype.start = function() {
+    if (this.running) 
+      throw new Error('Stopwatch has already started.');
+    
+    this.running = true; 
+
+    this.startTime = new Date();
+  };
+
+Stopwatch.prototype.stop = function() {
+    if (!this.running) 
+      throw new Error('Stopwatch is not started.');
+    
+    this.running = false; 
+
+    this.endTime = new Date();
+    
+    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+    this.duration += seconds; 
+};
+
+Stopwatch.prototype.reset = function() {
+    this.startTime = null;
+    this.endTime = null;
+    this.duration = 0;
+    this.unning = false;
+};
